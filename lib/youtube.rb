9d00c88be3d498f1a4de09886b9794174095d680
tomykaira <tomykaira@gmail.com>
2013-02-19 10:55:28 +0900

    Add retry feature to youtube

--- lib/youtube.rb
+++ lib/youtube.rb
@@ -6,23 +6,31 @@ require 'lib/youtube/url'
 module Youtube
   module_function
   def embedded_ids(url)
-    agent = Mechanize.new
-    agent.get(url)
-    agent.page.search('embed').map do |embed|
-      if embed.attributes['type'].value == "application/x-shockwave-flash"
-        embed.attributes['src'].value =~ /www\.youtube\.com\/v\/([^&?]*)/
-        $1
+    WorkerThread.new("youtube/embedded/#{url}") do
+      agent = Mechanize.new
+      retry_at_most(3) do
+        agent.get(url)
       end
-    end.compact
+      agent.page.search('embed').map do |embed|
+        if embed.attributes['type'].value == "application/x-shockwave-flash"
+          embed.attributes['src'].value =~ /www\.youtube\.com\/v\/([^&?]*)/
+          $1
+        end
+      end.compact
+    end
   end
 
   def run_thread(video_id)
     WorkerThread.new("youtube/video/#{video_id}") do
       Dir.mktmpdir do |dir|
         downloader = Video.new(video_id)
-        downloader.download(dir)
+        retry_at_most(3) do
+          downloader.download(dir)
+        end
 
-        DropboxUploader.new.upload_directory(dir)
+        retry_at_most(3) do
+          DropboxUploader.new.upload_directory(dir)
+        end
       end
     end
   end
